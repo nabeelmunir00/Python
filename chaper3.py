1327c7f866788af74c346e7ccffa71ff53e3a24a
Nabeel Munir <nabeelgaming786yt@gmail.com>
2026-07-04 16:56:14 +0500

    add data type explanations and examples

---
+++ chaper3.py
@@ -0,0 +1,26 @@
+# What is data type
+"""
+Every value in Python has a type that tells Python what kind of data it is and what you can do with it. You don't need to declare types — Python figures it out automatically.
+
+"""
+
+# The Main Data Types
+
+# interger => int 0 to infinity and -1 to negative infinity 
+
+# float => float 22.3  3/12 is ka float agher p/q ma ho to vo float he ho ga 
+# string => '' and "" and use ko string 
+# complex number => 3+4j is the example to complex example
+# boolean tyep => true and false
+
+age = 21
+name = 'nabeel'
+formula = 3 + 4j
+hasTrue = True
+version = 3.22
+
+print(type(age))
+
+
+
+# type() is use ko check type of variables
