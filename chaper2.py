7f216ff2f4a39a18dead32917aa68f3c11e51eef
Nabeel Munir <nabeelgaming786yt@gmail.com>
2026-07-04 16:36:16 +0500

    add comments and variable examples

---
+++ chaper2.py
@@ -0,0 +1,28 @@
+# Comment & Variables
+
+# =========== Comments ============ 
+
+# Comments has two types => single line comment & Multi Line Comment
+
+# Single Line Comment  => # sign use hota ha single line comment ka lea 
+
+# Multi Line Comment => 
+'''
+this is multi line comments
+'''
+
+# ======== Variables ======= 
+
+# variables type hota ha data ka gis ma hum na pata lata ha kon sa data kis ka ha 
+
+# Rlues of create a variables
+#1 can't start with number like 1name is not allow
+#2 space not allow like user name is not allow
+#3 did not user special characters like #@!$%&*()
+
+age = 13
+
+# Naming Conventions
+#camelCase  → myVariableName
+#PascalCase → MyVariableName
+#snake_case → my_variable_name
