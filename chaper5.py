71f42b2f0ee5e1d867b60174086979e04eb7f156
Nabeel Munir <nabeelgaming786yt@gmail.com>
2026-07-05 21:26:29 +0500

    add input handling and output display for user input value

--- chaper5.py
+++ chaper5.py
@@ -7,4 +7,6 @@ print(f"my name is {name} and my age is {age}")
 
 print("Hello my name is",name,"and my age is" ,age)
 
-input('Hello bro how are you!. Tell me what is   ')
+inputValue =  input('Hello bro how are you!:')
+
+print(f"Value of input: {inputValue}" )
