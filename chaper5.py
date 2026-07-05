035a20e0b3fdc24ab844ae1e9a006dd264b54b4d
Nabeel Munir <nabeelgaming786yt@gmail.com>
2026-07-05 20:43:30 +0500

    add initial implementation for input, output, and operators demonstration

---
+++ chaper5.py
@@ -0,0 +1,10 @@
+# Input, Output & Operators
+
+name = "nabeel"
+age = 21
+# format string 
+print(f"my name is {name} and my age is {age}")
+
+print("Hello my name is",name,"and my age is" ,age)
+
+input('Hello bro how are you!. Tell me what is   ')
