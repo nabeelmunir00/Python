c8b0f69c368bed63cdff92d4ad0e41af431437b4
Nabeel Munir <nabeelgaming786yt@gmail.com>
2026-07-06 12:54:13 +0500

    add arithmetic operations and explanations for basic math functions

--- chaper5.py
+++ chaper5.py
@@ -10,3 +10,31 @@ print("Hello my name is",name,"and my age is" ,age)
 inputValue =  input('Hello bro how are you!:')
 
 print(f"Value of input: {inputValue}" )
+
+# Arithmetic Operatores
+# number - int , float ,complex
+# (+,-,/,//,*,%,,**)
+
+value1 = 10
+value2 = 20
+
+print(value1 + value2) # => 10 + 20 = 30 
+print(value1 - value2) # => 10 - 20 = -10 
+print(value1 / value2) # => value return karta ha float ma
+
+# agher hum ko int ma value chaye. to hum // ka use kare ga 
+
+print(value1 // value2) # => int ma value return karta ha 
+
+print(value1* value2) # => 10 x 20 = 
+print(value1** value2) # => agher hum ko kis power add karna ha to hum  =
+
+print(value1 % value2) # hum ko mode return karta ha
+
+"""
+BoardMark Rules
+() -> Brackets
+** -> Exponent (right to left: 2**2**3 = 2**(2**3))
+* / // % -> Multiplication,Division,Floor Division
+"""
+
