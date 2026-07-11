53e5ed2d50046d05adf836fbb19d08cfd0f58c34
Nabeel Munir <nabeelgaming786yt@gmail.com>
2026-07-11 21:05:59 +0500

    refactor: comment out examples and explanations for for loops and range() function

--- chaper8.py
+++ chaper8.py
@@ -12,13 +12,60 @@
 # 23 - 56
 # 0 - 15
 
-range(10,101)
-range(23,57)
-range(0,46)
+# range(10,101)
+# range(23,57)
+# range(0,46)
 
-list(range(5))         # [0, 1, 2, 3, 4]
-list(range(1,6))       # [1, 2, 3, 4, 5]
-list(range(0,10,2))   # [0, 2, 4, 6, 8]
+# list(range(5))         # [0, 1, 2, 3, 4]
+# list(range(1,6))       # [1, 2, 3, 4, 5]
+# list(range(0,10,2))   # [0, 2, 4, 6, 8]
 
-for var in range(1,11):
-    print(f'5x{var} = {5 * var}')
+# for var in range(1,11):
+#     print(f'5x{var} = {5 * var}')
+
+
+# for loop with string
+
+# sti = 'Hello'
+# print("ok")
+# for i in sti:
+#     print(i)
+
+# for i in range(len(sti)):
+#     print(str[i])
+
+
+# solve for loop question 
+# Q1
+# Print "Hello World" n times.
+# Input: 3
+# Hello World × 3 lines
+
+# for i in range(1,4):
+#     print('Hello Word')
+
+# Q2
+# Print natural numbers from 1 to n.
+# Input: 5
+# 1 2 3 4 5
+
+num = int(input('Please enter a natural numbers to:-'))
+
+# for i in range(1,num+1):
+#     print(i)
+
+# Reverse for loop — print n down to 1.
+# Input: 5
+# 5 4 3 2 1
+
+# for i in range(num,0,-1):
+#     print(i)
+    
+
+#     Q4
+# Print the multiplication table of a number.
+# Input: 5
+# 5×1=5, 5×2=10 … 5×10=50
+
+# for i in range(1,11,1):
+#     print(f'{num}x{i}={num*i}')
