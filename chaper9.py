f4890fc6c8255501e677339a1f58c753083befe3
Nabeel Munir <nabeelgaming786yt@gmail.com>
2026-07-18 16:56:26 +0500

    refactor: comment out previous examples and add new example for reversing a number

--- chaper9.py
+++ chaper9.py
@@ -19,3 +19,23 @@
 #     count += 1
 
 # # Output: 1  2  3  4  5
+
+# eparate each digit of a number and print on a new line.
+# Input: 1234
+# 4 → 3 → 2 → 1
+
+num = int(input('please enter a number:'))
+
+# while num > 0:
+#     print(num%10)
+#     num = num // 10
+
+# Q2
+# Accept a number and print its reverse.
+# Input: 12345
+# 54321
+rev = 0
+while num > 0:
+    rev = rev * 10 + num % 10
+    num = num // 10
+print(rev)
