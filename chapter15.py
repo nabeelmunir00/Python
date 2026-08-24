9ca6fbdcd0b73434875a0780728ed6f28abf1b14
Nabeel Munir <nabeelgaming786yt@gmail.com>
2026-08-24 08:46:27 -0700

    - update dictionary example to demonstrate value retrieval and manipulation methods

--- chapter15.py
+++ chapter15.py
@@ -8,9 +8,13 @@ dic1 = {"name":"Nabeel","age":19} # or hum na key ko "" | '' ma write karna ha
 print(dic1)
 # or gis tara hum list or tuple ko index ka sate os ka data access karta ha ise tara dicitonary ma hum key ka use kar os ka data ko access kar sate ha 
 
-print(f"Name: {dic1['name']}")
+# print(f"Name: {dic1['name']}")
 
 dic1["Course"] = "BSSE" # is tara hum dictionary ma ak new value add kar sate ha key value ka sate
-
-print(dic1)
+dic1.update({"name":"Adil"}) # is tara hum kis be key value ko update kar sate agher key nahi ha to new value add ho gaye
+dic1.pop("name")
+print(dic1.values()) # dictionary kie sab values return karna ha without key 
+print(dic1.get("age"))  # os key kie value return kar ta ha jo hum os ko pass karte ha 
+print(dic1.items()) # all data return karta ha key and value dono
+print(dic1.clear()) # dicitonary ko empty kar data ha 
 
