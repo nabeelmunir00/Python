f11343035c5abd443803d8a3a6a5e0c639f93dbb
Nabeel Munir <nabeelgaming786yt@gmail.com>
2026-09-04 08:39:57 -0700

    feat: add examples for merging dictionaries and counting element frequency

--- chapter15.py
+++ chapter15.py
@@ -32,24 +32,74 @@ dic1.pop("name")
 # d1={a:1}, d2={b:2}
 # {a:1, b:2}
 
-d1 = {"a":10,"b":20}
-d2 = {"c":30,"d":40}
+# d1 = {"a":10,"b":20}
+# d2 = {"c":30,"d":40}
 
-for i in d2:
-    print(f"Key:{i} --> value:{d2[i]}")
-    d1[i] = d2[i]
+# for i in d2:
+#     print(f"Key:{i} --> value:{d2[i]}")
+#     d1[i] = d2[i]
 
-print(d1)
+# print(d1)
 # Q1
 # Merge two dictionaries into one.
 
 # d1={a:1}, d2={b:2}
 # {a:1, b:2}
-dics1 = {1:10,2:20,3:30}
-dics2 = {4:40,5:50}
+# dics1 = {1:10,2:20,3:30}
+# dics2 = {4:40,5:50}
 
-dics1.update(dics2)
+# dics1.update(dics2)
 
-print(f"New Data: {dics1}")
+# print(f"New Data: {dics1}")
+
+
+# Q2
+# Sum all values in a dictionary.
+# {"a":10,"b":20,"c":30}
+# Sum = 60
+
+# dicti1 = {"a":10,"b":20,"c":30,"d":40}
+# sum = 0
+
+# for i in dicti1:
+#     sum = sum + dicti1[i]
+
+# print(f"Total Sum: {sum}")
+
+
+
+# Q3
+# Count the frequency of each element in a list using a dictionary.
+# ["a","b","a","c","b","a"]
+# {"a":3,"b":2,"c":1}
+
+
+# l = ["a","b","a","a","a","b","c","c","b","b","d"]
+# d = {"a":5,"b":2}
+
+# for i in l:
+#     if i in d.keys():
+#         d[i] = d[i]+1
+#     else:
+#         d[i] = 1
+
+# print(d)
+
+
+# Q4
+# Combine two dicts, adding values for common keys.
+# d1={a:5,b:3}, d2={b:4,c:2}
+# {a:5, b:7, c:2}
+d1 = {"a":5,"b":5,"c":4}
+d2 = {"b":4,"c":2}
+
+for i in d2:
+    if i in d2.keys():
+        d1[i] = d1[i] + d2[i]
+    else:
+        d1[i] = d1[i]+d2[i]
+
+print(d1)
+
 
 
